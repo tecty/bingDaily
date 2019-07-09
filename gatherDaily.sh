d4968b405b2d023dd3b1e5558fbea1a7d6c17009
Toby HUANG <z5141448@ad.unsw.edu.au>
2019-07-09 10:42:41 +1000

    something for publish

--- gatherDaily.sh
+++ gatherDaily.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/bin/sh
 # this can be add to cron as a daily job to autmaticcally update 
 ./getUrls.py > daily.dat
 cat  all.dat daily.dat | sort | uniq > all.dat
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/sh
+#!/bin/sh
 source venv/bin/activate 
 
 export FLASK_APP=server.py
 export FLASK_ENV=development
-flask run --with-threads --no-debugger 
+flask run --with-threads --no-debugger -h 0.0.0.0
